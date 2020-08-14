31c664b53f14719a2f1623e83af7ad7e366be194
TaikiInoue <taikiinoue45@gmail.com>
2020-08-15 00:40:30 +0900

    Fix for github actions

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,2 @@
-cp /tmp/.ssh /root/.ssh -r
-mkdir -p /dgx/github
-mkdir -p /dgx/data
-mkdir -p /dgx/shared/momo
-sshfs inoue.momo:/dgx/inoue/github/ /dgx/github
-sshfs inoue.momo:/dgx/inoue/data/ /dgx/data
-sshfs inoue.momo:/dgx/shared/ /dgx/shared/momo
-cd /app/github_actions/DVC
-dvc pull data/202008131442_H.zip.dvc
-dvc repro
 cd /app/github_actions/SSAD/ssad
 python run.py conf/config.yaml
