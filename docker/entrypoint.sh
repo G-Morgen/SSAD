f388571b49490556b4e5654e8371f4cda1834b18
TaikiInoue <taikiinoue45@gmail.com>
2020-08-14 16:32:56 +0900

    Support github actions

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,8 @@ mkdir -p /dgx/shared/momo
 sshfs inoue.momo:/dgx/inoue/github/ /dgx/github
 sshfs inoue.momo:/dgx/inoue/data/ /dgx/data
 sshfs inoue.momo:/dgx/shared/ /dgx/shared/momo
-/usr/bin/zsh
+cd /app/github_actions/DVC
+dvc pull data/202008131442_H.zip.dvc
+dvc repro
+cd /app/github_actions/SSAD/ssad
+python run.py conf/config.yaml
